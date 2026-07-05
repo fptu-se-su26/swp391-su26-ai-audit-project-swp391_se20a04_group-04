2c1eaced543a428954c2b8ac397505a5202dfea6
Tuan Van <tuanvan01042005@gmail.com>
2026-07-06 00:06:05 +0700

    fix

--- .src/backend/config/firebase.js
+++ .src/backend/config/firebase.js
@@ -10,6 +10,9 @@ let isInitialized = false;
 if (process.env.SERVICE_ACCOUNT_JSON) {
   try {
     const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_JSON);
+    if (serviceAccount.private_key) {
+      serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+    }
     admin.initializeApp({
       credential: admin.credential.cert(serviceAccount)
     });
